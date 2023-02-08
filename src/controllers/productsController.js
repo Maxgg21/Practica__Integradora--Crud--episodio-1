f4f198322d2f08bbf596b9cf446a323d55f1763f
Maxgg21 <lionel.gegena@gmail.com>
2023-02-07 21:19:33 -0300

    Se agrego la funcionalidad de crear, editar, elimiar un producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const writeJson = (products) => {
+	fs.writeFileSync(productsFilePath, JSON.stringify(products), {
+		encoding: 'utf-8'
+	})
+}
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -34,20 +39,63 @@ const controller = {
 	// Create -  Method to store
 	store: (req, res) => {
 		// Do the magic
+
+		let lastId = products[products.length - 1].id
+
+		newProduct = {
+			id:lastId + 1,
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			image: "default-image.png"
+		}
+		products.push(newProduct);
+
+		writeJson(products);
+		res.redirect('/products/')
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		res.render('product-edit-form');
+		// Do the magic
+		let productId = Number(req.params.id);
+
+		let productToEdit = products.find(product => product.id === productId);
+
+		res.render('product-edit-form', {
+			productToEdit
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
 		// Do the magic
+		let productId = Number(req.params.id)
+
+		products.forEach(product => {
+			if(product.id === productId) {
+			product.name = req.body.name,
+			product.price = req.body.price,
+			product.discount = req.body.discount,
+			product.category = req.body.category,
+			product.description = req.body.description
+			}
+		});
+
+		writeJson(products);
+		res.redirect('/products/')
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
 		// Do the magic
+		let productId = Number(req.params.id)
+
+		let newProductArray = products.filter(product => product.id != productId)
+
+		writeJson(newProductArray)
+		res.send('Producto Eliminado')
 	}
 };
 
